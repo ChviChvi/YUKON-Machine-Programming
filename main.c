b90a3489d6f8a0d482086127cf14ece5a26e51fa
Christiaan Vink <christiaan_vink@outlook.com>
2022-04-26 22:02:19 +0200

    updating whatever

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <time.h>
+#include <string.h>
+
 /** Creates the node structure */
 typedef struct Node {
     char *data;
@@ -24,10 +26,6 @@ typedef struct Deck{
 //      it probably has to go like; linked lists which than have to reatach from one spot to another spot
 //      fx command could be; 3D TO 8H
 
-typedef struct private private;
-
-typedef struct data data;
-
 // print the linked list value
 char plist(struct Node *p, int n) {
     int k = 0;
@@ -45,23 +43,117 @@ char plist(struct Node *p, int n) {
         }
         p = p->next;
     }
+}
+char pointer(struct Node *p, int n) {
+    int k = 0;
 
+    while (p != NULL) {
+        if(k == n) {
+        return p->data;
+            }
+        p = p->next;
+    }
+}
+void display( const struct Node *node ) {
+    while (node != NULL) {
+        printf(" %s ", node->data);
+        node = node->next;
+    }
+}
+void displaye( const struct Node *node ) {
+    while (node != NULL) {
+        printf(" %d ", node->data);
+        node = node->next;
+    }
+}
+void deleteNode(struct Node** head_ref, int key)
+{
+    struct Node *temp = *head_ref, *prev;
 
+    if (temp != NULL && temp->data == key) {
+        *head_ref = temp->next; // Changed head
+        free(temp); // free old head
+        return;
+    }
 
-}//ok
+    // Search for the key to be deleted, keep track of the
+    // previous node as we need to change 'prev->next'
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    // If key was not present in linked list
+    if (temp == NULL)
+        return;
+
+    // Unlink the node from linked list
+    prev->next = temp->next;
+
+    free(temp); // Free memory
+}
 
 void one(struct Node *head);
 Node * createLinkedlistS();
-//void printList(struct Node* n);
+void printList(struct Node* n);
+Node * heartLinkedlistS(char n);
+
+Node *kindlistS(int n);
+void push(struct Node** head_ref, char new_data);
+
 
 int main() {
 
     /** Initializing nodes and allocating memory */
     Node * head = NULL;
-
     head = createLinkedlistS();
+
+    /** Collumns */
+    struct Node* C1 = NULL;
+    struct Node* C2 = NULL;
+    struct Node* C3 = NULL;
+    struct Node* C4 = NULL;
+    struct Node* C5 = NULL;
+    struct Node* C6 = NULL;
+    struct Node* C7 = NULL;
+    /** Decks */
+    struct Node* F1 = NULL;
+    struct Node* F2 = NULL;
+    struct Node* F3 = NULL;
+    struct Node* F4 = NULL;
+
+    Node * SPADES = NULL;
+    SPADES = kindlistS(1);
+    display(SPADES);
+    printf("\n");
+
+    Node * HEARTS = NULL;
+    HEARTS = kindlistS(2);
+    display(HEARTS);
+    printf("\n");
+
+    Node * DIAMONDS = NULL;
+    DIAMONDS = kindlistS(3);
+    display(DIAMONDS);
+    printf("\n");
+
+    Node * CLUBS = NULL;
+    CLUBS = kindlistS(4);
+    display(CLUBS);
+    printf("\n");
+    //qlist(p.head2);
     //plist(head,5);
-    //printLinkedlist(head);
+
+    push(&C1, "hi");
+    push(&C1, "hi");
+    push(&C1, "hi");
+    push(&C1, "hi");
+    display(C1);
+    deleteNode(&C1,3);
+
+    display(C1);
+    printf("\n");
+    //delete(pointer(CLUBS,6));
 
 
     printf("\n");
@@ -233,4 +325,147 @@ void one(struct Node *head)
         current = current->next;
     }
     printf(result);
+}
+
+
+Node * kindlistS(int n){
+
+
+    struct Node* head;
+    /**SPADES*/
+    struct Node* ACES = NULL; ACES = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* TWOS = NULL; TWOS = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* THREES = NULL; THREES = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* FOURS = NULL; FOURS = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* FIVES = NULL; FIVES = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* SIXS = NULL; SIXS = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* SEVENS = NULL; SEVENS = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* EIGHTS = NULL; EIGHTS = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* NINES = NULL; NINES = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* TENS = NULL; TENS = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* FARMERS = NULL; FARMERS = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* QUEENS = NULL; QUEENS = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* KINGS = NULL; KINGS = (struct Node*)malloc(sizeof(struct Node));
+    /**HEARTS*/
+    struct Node* ACEH = NULL; ACEH = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* TWOH = NULL; TWOH = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* THREEH = NULL; THREEH = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* FOURH = NULL; FOURH = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* FIVEH = NULL; FIVEH = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* SIXH = NULL; SIXH = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* SEVENH = NULL; SEVENH = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* EIGHTH = NULL; EIGHTH = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* NINEH = NULL; NINEH = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* TENH = NULL; TENH = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* FARMERH = NULL; FARMERH = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* QUEENH = NULL; QUEENH = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* KINGH = NULL; KINGH = (struct Node*)malloc(sizeof(struct Node));
+    /**DIAMONDS*/
+    struct Node* ACED = NULL; ACED = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* TWOD = NULL; TWOD = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* THREED = NULL; THREED = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* FOURD = NULL; FOURD = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* FIVED = NULL; FIVED = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* SIXD = NULL; SIXD = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* SEVEND = NULL; SEVEND = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* EIGHTD = NULL; EIGHTD = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* NINED = NULL; NINED = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* TEND = NULL; TEND = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* FARMERD = NULL; FARMERD = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* QUEEND = NULL; QUEEND = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* KINGD = NULL; KINGD = (struct Node*)malloc(sizeof(struct Node));
+    /**CLUBS*/
+    struct Node* ACEC = NULL; ACEC = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* TWOC = NULL; TWOC = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* THREEC = NULL; THREEC = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* FOURC = NULL; FOURC = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* FIVEC = NULL; FIVEC = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* SIXC = NULL; SIXC = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* SEVENC = NULL; SEVENC = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* EIGHTC = NULL; EIGHTC = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* NINEC = NULL; NINEC = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* TENC = NULL; TENC = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* FARMERC = NULL; FARMERC = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* QUEENC = NULL; QUEENC = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* KINGC = NULL; KINGC = (struct Node*)malloc(sizeof(struct Node));
+
+    /** Initializing nodes and allocating memory */
+    /**SPADES*/
+    ACES->data = "AS"; ACES->next = TWOS;
+    TWOS->data = "2S"; TWOS->next = THREES;
+    THREES->data = "3S"; THREES->next = FOURS;
+    FOURS->data = "4S"; FOURS->next = FIVES;
+    FIVES->data = "5S"; FIVES->next = SIXS;
+    SIXS->data = "6S"; SIXS->next = SEVENS;
+    SEVENS->data = "7S"; SEVENS->next = EIGHTS;
+    EIGHTS->data = "8S"; EIGHTS->next = NINES;
+    NINES->data = "9S"; NINES->next = TENS;
+    TENS->data = "10S"; TENS->next = FARMERS;
+    FARMERS->data = "JS"; FARMERS->next = QUEENS;
+    QUEENS->data = "QS"; QUEENS->next = KINGS;
+    KINGS->data = "KS"; KINGS->next = NULL;
+    /**HEARTS*/
+    ACEH->data = "AH"; ACEH->next = TWOH;
+    TWOH->data = "2H"; TWOH->next = THREEH;
+    THREEH->data = "3H"; THREEH->next = FOURH;
+    FOURH->data = "4H"; FOURH->next = FIVEH;
+    FIVEH->data = "5H"; FIVEH->next = SIXH;
+    SIXH->data = "6H"; SIXH->next = SEVENH;
+    SEVENH->data = "7H"; SEVENH->next = EIGHTH;
+    EIGHTH->data = "8H"; EIGHTH->next = NINEH;
+    NINEH->data = "9H"; NINEH->next = TENH;
+    TENH->data = "10H"; TENH->next = FARMERH;
+    FARMERH->data = "JH"; FARMERH->next = QUEENH;
+    QUEENH->data = "QH"; QUEENH->next = KINGH;
+    KINGH->data = "KH"; KINGH->next = NULL;
+    /**DIAMONDS*/
+    ACED->data  = "AD"; ACED->next = TWOD;
+    TWOD->data = "2D"; TWOD->next = THREED;
+    THREED->data = "3D"; THREED->next = FOURD;
+    FOURD->data = "4D"; FOURD->next = FIVED;
+    FIVED->data = "5D"; FIVED->next = SIXD;
+    SIXD->data = "6D"; SIXD->next = SEVEND;
+    SEVEND->data = "7D"; SEVEND->next = EIGHTD;
+    EIGHTD->data = "8D"; EIGHTD->next = NINED;
+    NINED->data = "9D"; NINED->next = TEND;
+    TEND->data = "10D"; TEND->next = FARMERD;
+    FARMERD->data = "JD"; FARMERD->next = QUEEND;
+    QUEEND->data = "QD"; QUEEND->next = KINGD;
+    KINGD->data = "KD"; KINGD->next = NULL;
+    /**CLUBS*/
+    ACEC->data = "AC"; ACEC->next = TWOC;
+    TWOC->data = "2C"; TWOC->next = THREEC;
+    THREEC->data = "3C"; THREEC->next = FOURC;
+    FOURC->data = "4C"; FOURC->next = FIVEC;
+    FIVEC->data = "5C"; FIVEC->next = SIXC;
+    SIXC->data = "6C"; SIXC->next = SEVENC;
+    SEVENC->data = "7C"; SEVENC->next = EIGHTC;
+    EIGHTC->data = "8C"; EIGHTC->next = NINEC;
+    NINEC->data = "9C"; NINEC->next = TENC;
+    TENC->data = "10C"; TENC->next = FARMERC;
+    FARMERC->data = "JC"; FARMERC->next = QUEENC;
+    QUEENC->data = "QC"; QUEENC->next = KINGC;
+    KINGC->data = "KC"; KINGC->next = NULL;
+
+    if (n==1) {
+        head = ACES;
+    }
+    if (n==2) {
+        head = ACEH;
+    }
+    if (n==3) {
+        head = ACED;
+    }
+    if (n==4) {
+        head = ACEC;
+    }
+}
+
+
+void push(struct Node** head_ref, char new_data)
+{
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+    new_node->data = new_data;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
 }
